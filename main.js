8c48aecf159cd898f34f37c016316c1d1dd46ee6
nasir733 <engnasir733@gmail.com>
2020-11-30 13:03:35 +0500

    playing with es6

---
+++ main.js
@@ -0,0 +1,34 @@
+// const names = ["name", "nasir", "nico"];
+
+// const hearts = names.map(function (item) {
+//   return item + "😋";
+// });
+// console.log(hearts);
+// the map functions goes over all the things in a  list and performa a functoins on it for example the above code add a smile emjoy on the every name in the list
+// const names = ["name", "nasir", "nico"];
+
+// const hearts = names.map((item, index) => {
+//   return item + "😋" + index;
+// });
+// console.log(hearts);
+// const names = ["name", "nasir", "nico"];
+
+// const hearts = names.map((item, index) => item + "😋" + index); // implicit return
+// console.log(hearts);
+
+const email = [
+  "engnasir7333@gmail.com",
+  "techcounter3@gmcail.com",
+  "nasir@nasir.com",
+];
+// const foundMail = email.find((item) => item.includes("@nasir.com"));
+// console.log(foundMail);
+
+// const noGmail = email.filter((email) => !email.includes("gmail.com"));
+// console.log(noGmail);
+// const cle = email.map((email, index) => ({
+//   username: email.split("@")[0],
+//   points: index,
+// }));
+// console.table(cle);
+//to make the arrow function explicit retrun on an dict and "("around it this will return the explicit return
